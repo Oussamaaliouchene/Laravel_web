b719e106be60a773b7e3f1f1bea288106c282479
oussama <aliouchne123@gmail.com>
2023-11-08 14:32:47 +0100

    lecteur 5

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -76,34 +76,32 @@
 					<div class=" main-content-area">
 						<div class="wrap-login-item ">
 							<div class="register-form form-item ">
+								<x-validation-errors class="mb-4" />
 								<form class="form-stl" action="{{route('register')}}" name="frm-login" method="POST" >
+									@csrf
 									<fieldset class="wrap-title">
 										<h3 class="form-title">Create an account</h3>
 										<h4 class="form-subtitle">Personal infomation</h4>
 									</fieldset>									
 									<fieldset class="wrap-input">
 										<label for="frm-reg-lname">Name</label>
-										<input type="text" id="frm-reg-lname" name="reg-lname" placeholder="Last name*">
+										<input type="text" id="frm-reg-lname" name="name" placeholder="Your name" :value=name required autofocus autocomplete="name">
 									</fieldset>
 									<fieldset class="wrap-input">
 										<label for="frm-reg-email">Email Address</label>
-										<input type="email" id="frm-reg-email" name="reg-email" placeholder="Email address">
+										<input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value=email>
 									</fieldset>
-									<fieldset class="wrap-functions ">
-										<label class="remember-field">
-											<input name="newletter" id="new-letter" value="forever" type="checkbox"><span>Sign Up for Newsletter</span>
-										</label>
-									</fieldset>
+									
 									<fieldset class="wrap-title">
 										<h3 class="form-title">Login Information</h3>
 									</fieldset>
 									<fieldset class="wrap-input item-width-in-half left-item ">
 										<label for="frm-reg-pass">Password</label>
-										<input type="text" id="frm-reg-pass" name="reg-pass" placeholder="Password">
+										<input type="password" id="frm-reg-pass" name="password" placeholder="Password" required autocomplete="new-password">
 									</fieldset>
 									<fieldset class="wrap-input item-width-in-half ">
 										<label for="frm-reg-cfpass">Confirm Password *</label>
-										<input type="text" id="frm-reg-cfpass" name="reg-cfpass" placeholder="Confirm Password">
+										<input type="password" id="frm-reg-cfpass" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
 									</fieldset>
 									<input type="submit" class="btn btn-sign" value="Register" name="register">
 								</form>
